5aafd4ffb806b0bf4136c9611737f49fc74b6a21
eferreyro <emafe7472@gmail.com>
2021-05-24 17:24:19 -0500

    agregando route a posts y tags para enlistar dinamicamente los componentes

--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 py-8">
-        <h1 class="text-3xl text-gray-600 font-bold text-center uppercase">Categoria: {{$category->name}}</h1>
+        <h1 class="text-3xl text-gray-600 font-bold text-center uppercase">Etiqueta: {{$tag->name}}</h1>
         @foreach ($posts as $post)
         {{-- Vinculo el componente card-post.blade.php y le paso la variable $post para enviarle los datos del foreach --}}
           <x-card-post :post="$post" />
